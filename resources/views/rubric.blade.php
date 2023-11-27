58f549805d7bca4c7a163e494c372337d6c4b2fb
afikri124 <afikri124@gmail.com>
2023-11-27 10:54:37 +0700

    update rubric

--- resources/views/rubric.blade.php
+++ resources/views/rubric.blade.php
@@ -25,10 +25,10 @@
                <h5>Rubric Guidance</h5>
             </div>
             <div class="d-none d-md-block">
-                <iframe src="{{asset('assets/rubric.pdf')}}" style="width:100%; height:650px;" frameborder="0"></iframe>
+                <iframe src="{{asset('assets/rubric_new.pdf')}}" style="width:100%; height:650px;" frameborder="0"></iframe>
             </div>
             <div class="text-center pb-4">
-                <a href="{{asset('assets/rubric.pdf')}}" class="btn btn-primary" target="_blank">Download</a>
+                <a href="{{asset('assets/rubric_new.pdf')}}" class="btn btn-primary" target="_blank">Download</a>
             </div>
          </div>
       </div>
